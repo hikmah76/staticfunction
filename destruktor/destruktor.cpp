dafd87b23fd8bd99392ce6b5281d941b78fa7e6c
nur hikmah <nur.hikmah190504@gmail.com>
2023-05-15 19:40:05 +0700

    destruktor

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -25,23 +25,3 @@ angka::~angka() { //destructor
 	cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
 
-void angka::cetakData() {
-	for (int i = 1; i <= panjang; i++) {
-		cout << i << " = " << arr[i] << endl;
-	}
-}
-
-void angka::isiData() {
-	for (int i = 1; i < panjang; i++) {
-		cout << i << " = "; cin >> arr[i];
-	}
-	cout << endl;
-}
-
-int main() {
-	angka belajarcpp(3); //constructor dipanggil
-	angka* ptrBelajarcpp = new angka(5); //constructor dipanggil
-	delete ptrBelajarcpp; //constructor dipanggil
-
-	return 0;
-}//constructor dipanggil
