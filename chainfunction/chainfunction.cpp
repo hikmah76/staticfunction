01ce109444c14f29eefd08425f55bd2d41607344
nur hikmah <nur.hikmah190504@gmail.com>
2023-05-15 19:25:36 +0700

    staticfunction

---
+++ chainfunction/chainfunction.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class buku
+{
+	string judul;
+public:
+	buku setJudul(string judul)
+	{
+		this->judul = judul;
+		return *this; //chain function
+	}
+	string getJudul()
+	{
+		return this->judul;
+	}
+} bukunya;
+
+
+int main()
+{
+	//bukunya.setJudul("Matematika");
+	//cout << bukunya.detJudul();
+	cout << bukunya.setJudul("Matematika").getJudul(); //chain function
+	return 0;
+}
---
+++ staticfunction2/staticfunction2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNama(int pNim) { nim = pNim; /*Definisi Function*/ }
+	static int getNim() { return nim; /*Definisi Function*/ }
+
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID =" << id << endl;
+	cout << "Nama =" << nama << endl;
+	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Sri Dadi");
+	mahasiswa mhs2("Budi Jatmiko");
+
+	mahasiswa mhs3("Andi Janu");
+	mahasiswa mhs4("Joko Wahono");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "Akses dari luar objek =" << mahasiswa::getNim() << endl; //mengakses nim
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+//mengakses nim melalui static member function "setNim"
+
